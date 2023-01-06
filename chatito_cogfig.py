9b334815fd9b1efeeac19ab8e220a4bece9de2d8
Nadia Hawa <nadia.h.hawa@gmail.com>
2023-01-06 11:09:29 -0500

    Main

---
+++ chatito_cogfig.py
@@ -0,0 +1,28 @@
+# %[findByGroupLeaders]('training': '1000', 'testing': '100')
+#     ~[find?] ~[groupleaders?] ~[in?] ~[cityslot] ~[for] ~[typelaw]
+
+# ~[find]
+#     find
+#     search
+#     lookup
+
+# ~[in]
+#     around
+#     nearby
+#     in the area of
+#     at
+
+# ~[for]
+#     for
+
+# ~[groupleaders]
+#     groupleaders
+#     leaders
+#     leader
+#     group leader
+
+# ~[typelaw]
+#     [[cannabis law|laws]]
+
+# ~[cityslot]
+#     [[chesterbrook|city]]
